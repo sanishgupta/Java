8cf3e90c84f4a886ed06967216b93eb0861327e4
SanishGupta <sanish.lnct@gmail.com>
2018-06-18 17:12:23 +0400

    update in 7 java project classpath

--- Java8_Tutorial/src/com/dev/java8/part1/ChainConsumers.java
+++ Java8_Tutorial/src/com/dev/java8/part1/ChainConsumers.java
@@ -7,6 +7,7 @@ import java.util.function.Consumer;
 
 public class ChainConsumers {
 
+	
 	public static void main(String... args) {
 
 		List<String> strings = Arrays.asList("one", "two", "three", "four", "five");
